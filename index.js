fbcb61ea7c60d3874ce7db318fd5305cb53a10b2
Harrison Kamau <harrison@linux-sinr.suse>
2015-11-19 13:57:27 +0300

    Added more dependencies

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var http = require('http');
 var dispatch = require('dispatch');
 var querystring = require('querystring');
+//express 
+var express = require('express');
+var app = express(); //invoking express
+
+//express middleware
+var bodyParser = require('body-parser');
+
+
+
 //include mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/terrific-tuesday');
@@ -12,12 +21,10 @@ var movieSchema = mongoose.Schema({
 
 // compile our model
 var Movie = mongoose.model('Movie', movieSchema);
+app.use(bodyParser.urlencoded({extended: true}));
 
-
-var server = http.createServer(
-			dispatch ({
-			'/movies' :{
-				'GET /' : function(request,response,next){
+app.get('/movies',function(req, res){
+	
 							movies = [
 							{
 								title: 'Dangerous Arrangement',
@@ -41,44 +48,101 @@ var server = http.createServer(
 
 
 							]; 
-							response.writeHead(200, {'Content-Type': 'application/json'})
-							
-							response.end(JSON.stringify(movies));
-				},
-				'POST /': function(request, response, next){
-					//Get parameters from the form
-					var formData = '';
-					request.on('data', function(chunk){
-						formData = querystring.parse(chunk.tostring());
-					});
-					request.on('end', function(){
-
-
-					console.log(formData);
-
-					//create an instance of a movie
-					var movie = new Movie(
+							res.json(movies);
+						});
+app.post('/movies', function(req, res){
+	console.log(req.body);
+	formData = req.body;
+	var movie = new Movie(
 					{
-						title: '',
-						year_of_release: ''
+						title: formData.title,
+						year_of_release: formData.year_of_release
 
 					}
 				);
-					//save the movie instance
-					//If successful respond with the saved movie instance
-				});
+	var movie = new Movie(formData);
+	movie.save(function(err, movie){
+
+	});
+
+});
+
+
+
+// var server = http.createServer(
+// 			dispatch ({
+// 			'/movies' :{
+// 				'GET /' : function(request,response,next){
+// 							movies = [
+// 							{
+// 								title: 'Dangerous Arrangement',
+// 								Category: ['Thriller', 'Drama','Mystery'],
+// 								main_actors: [{
+// 									first_name: 'Amy',
+// 									last_name: 'Ellen'
+// 								},{}]
+// 							},
+// 							{
+// 								title: 'Scorpion',
+// 								Category: ['Mystery','Drama','Thriller'],
+// 								main_actors: [{
+// 									first_name: 'Walter',
+// 									last_name: 'Brien'
+// 								},{
+// 									first_name: 'Cape',
+// 									last_name: 'Galloy'
+// 								}]
+// 							}
+
+
+// 							]; 
+// 							response.writeHead(200, {'Content-Type': 'application/json'})
+							
+// 							response.end(JSON.stringify(movies));
+// 				},
+// 				'POST /': function(request, response, next){
+// 					//Get parameters from the form
+// 					var formData ='';
+// 					request.on('data', function(chunk){
+// 						formData = querystring.parse(chunk.toString());
+// 					});
 					
-				}
+// 					request.on('end', function(){
+// 						console.log(formData);
 
-		}
+					
+
+// 					//create an instance of a movie
+// 					var movie = new Movie(
+// 					{
+// 						title: formData.title,
+// 						year_of_release: formData.year_of_release
+
+// 					}
+// 				);
+					
+// 					response.end();
+// 					// console.log(movie);
+					
+// 					//save the movie instance
+// 					//If successful respond with the saved movie instance
+// 					});
+
+
+
+					
+// 					}
+
+
+// 			}
 	
-	})
-);
+// 			})
+// );
 	
 
 
 
-server.listen(8081, function(){
+app.listen(8081, function(){
 console.log('Server running on 127.0.0.1:8081');
 });
 
