88606a8d9cae46ebb37839593fb3015bc4d1cb0d
Harrison Kamau <harrison@linux-sinr.suse>
2015-12-02 09:37:12 +0300

    added a snippet to the .gitignore

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var http = require('http');
-var dispatch = require('dispatch');
-var querystring = require('querystring');
+
 //express 
 var express = require('express');
-var app = express(); //invoking express
+var cors = require('express-cors')
+
+//invoking express
+var app = express(); 
 
 //express middleware
 var bodyParser = require('body-parser');
 
+ 
+app.use(cors({
+    allowedOrigins: [
+        '127.0.0.1:8080'
+    ]
+}));
 
 
 //include mongoose
@@ -22,6 +29,7 @@ var movieSchema = mongoose.Schema({
 // compile our model
 var Movie = mongoose.model('Movie', movieSchema);
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/movies',function(req, res){
 	
@@ -48,8 +56,16 @@ app.get('/movies',function(req, res){
 
 
 							]; 
-							res.json(movies);
+							res.redirect('http://www.google.com');
+							// res.json(movies);
 						});
+
+
+
+
+
+
+
 app.post('/movies', function(req, res){
 	console.log(req.body);
 	formData = req.body;
@@ -67,125 +83,6 @@ app.post('/movies', function(req, res){
 
 });
 
-
-
-// var server = http.createServer(
-// 			dispatch ({
-// 			'/movies' :{
-// 				'GET /' : function(request,response,next){
-// 							movies = [
-// 							{
-// 								title: 'Dangerous Arrangement',
-// 								Category: ['Thriller', 'Drama','Mystery'],
-// 								main_actors: [{
-// 									first_name: 'Amy',
-// 									last_name: 'Ellen'
-// 								},{}]
-// 							},
-// 							{
-// 								title: 'Scorpion',
-// 								Category: ['Mystery','Drama','Thriller'],
-// 								main_actors: [{
-// 									first_name: 'Walter',
-// 									last_name: 'Brien'
-// 								},{
-// 									first_name: 'Cape',
-// 									last_name: 'Galloy'
-// 								}]
-// 							}
-
-
-// 							]; 
-// 							response.writeHead(200, {'Content-Type': 'application/json'})
-							
-// 							response.end(JSON.stringify(movies));
-// 				},
-// 				'POST /': function(request, response, next){
-// 					//Get parameters from the form
-// 					var formData ='';
-// 					request.on('data', function(chunk){
-// 						formData = querystring.parse(chunk.toString());
-// 					});
-					
-// 					request.on('end', function(){
-// 						console.log(formData);
-
-					
-
-// 					//create an instance of a movie
-// 					var movie = new Movie(
-// 					{
-// 						title: formData.title,
-// 						year_of_release: formData.year_of_release
-
-// 					}
-// 				);
-					
-// 					response.end();
-// 					// console.log(movie);
-					
-// 					//save the movie instance
-// 					//If successful respond with the saved movie instance
-// 					});
-
-
-
-					
-// 					}
-
-
-// 			}
-	
-// 			})
-// );
-	
-
-
-
 app.listen(8081, function(){
 console.log('Server running on 127.0.0.1:8081');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//create a schema
-// var pizzaSchema = mongoose.Schema({
-// 	name: String,
-// 	price: Number,
-// 	created_at: {type: Date, default: Date.now()}
-// });
-
-// //compile our model
-// var Pizza = mongoose.model('Pizza', pizzaSchema);
-
-// //using the model
-// var pizza = new Pizza({name: 'Vegeterian', price: 1000});
-
-// //create the document
-// pizza.save(function(err, pizza){
-// 	if (err){
-// 		 return console.error('Your pizza was not saved', pizza);
-// 	}
-// 	console.log('Your pizza was successfully saved:)');
-
-// });
