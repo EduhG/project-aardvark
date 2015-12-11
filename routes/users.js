651f283d6d3ed7be74a90a6b28567a72c866b836
Harrison Kamau <harrison@linux-sinr.suse>
2015-12-11 16:53:44 +0300

    Added users.js && user.js && sign-up.html

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,16 @@ router.route('/sign-up')
 		.get(function(req, res){
 			res.render('users/sign-up');
 		})
-	    .post(function(req, res) {
-	        var username = req.body.username;
-	        var password = req.body.password;
-	        res.json(
-	            { 
-	               message: 'signup success',
-	                username : username,
-	                 password : password,
-	            }
-	        );
-	    });
+	    // .post(function(req, res) {
+	    //     var username = req.body.username;
+	    //     var password = req.body.password;
+	    //     res.json(
+	    //         { 
+	    //            message: 'signup success',
+	    //             username : username,
+	    //              password : password,
+	    //         }
+	    //     );
+	    // });
    
 module.exports = router;
